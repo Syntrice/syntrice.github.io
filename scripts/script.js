cf7139c3b77f35ae07d8fbf6543a5e254ecbddf0
Syntrice <Syntrice@users.noreply.github.com>
2025-01-19 12:32:27 +0000

    deploy: 50e849200291a2ad5ac695684a577a63cca61573

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function toggleNavbar() {
-    var x = document.getElementById("site-navbar");
-    if (x.className === "navbar") {
-      x.className += " hidden";
+    var x = document.getElementById("site-navbar-list");
+    if (x.className === "navbar-list") {
+        x.className += " hidden";
     } else {
-      x.className = "navbar";
+        x.className = "navbar-list";
     }
-  }
+}
